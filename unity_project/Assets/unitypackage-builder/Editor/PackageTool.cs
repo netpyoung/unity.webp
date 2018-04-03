9afaf9053c55adcd927c6fc1b1c4077e49a7971b
netpyoung <netpyoung@gmail.com>
2018-04-03 21:25:24 +0800

    ver 0.0.2

--- unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
+++ unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
@@ -1,16 +1,15 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
 
 public class PackageTool
 {
-	[MenuItem("Package/Update Package")]
-	static void UpdatePackage()
-	{
-		const string VERSION = "0.0.1";
-		AssetDatabase.ExportPackage(
-			new string[] {"Assets/unity.webp"},
-			string.Format("../webp-{0}.unitypackage", VERSION),
-			ExportPackageOptions.Recurse
-		);
-	}
+    [MenuItem("Package/Update Package")]
+    private static void UpdatePackage()
+    {
+        const string VERSION = "0.0.2";
+        AssetDatabase.ExportPackage(
+            new[] {"Assets/unity.webp"},
+            $"../webp-{VERSION}.unitypackage",
+            ExportPackageOptions.Recurse
+        );
+    }
 }
