1d827e5d19a6a10f0cf4b7730f7b75dff38296b6
shunia <shunia@qq.com>
2020-07-14 11:42:55 +0800

    Put all code together; Added example for simple usage; Adjusted the comments for easier understandings;

---
+++ unity_project/Assets/unity.webp/WebPDecodeJob.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Unity.Jobs;
+using UnityEngine;
+using WebP;
+using WebP.Extern;
+
+namespace WebP
+{
+
+    /// <summary>
+    /// WebP decoding process which optimized by using threads.
+    /// </summary>
+    public static class WebPDecodeJob
+    {
+
+        public static async Task<List<(byte[], int)>> StartJob(IntPtr decoder, int count)
+        {
+            Debug.Log($"[WebPDecodeJob] Starting multi-threading webp decode");
+            var managedBytes = new ConcurrentDictionary<int, (byte[], int)>();
+            var task = new WebPDecodeTask(decoder, managedBytes);
+            var taskRef = GCHandle.Alloc(task);
+            var job = new WebPDecoderJob {Task = taskRef};
+            var handler = job.Schedule(count, 1);
+            var handled = false;
+        
+            while (!handled)
+            {
+                handled = handler.IsCompleted;
+            }
+            
+            handler.Complete();
+            taskRef.Free();
+
+            if (managedBytes.Count <= 0 || managedBytes.Values.Count <= 0) return new List<(byte[], int)>();
+            
+            var sorted = managedBytes.OrderBy(kvp => kvp.Key).ToList().ConvertAll(kvp => kvp.Value);
+            return sorted;
+        }
+    }
+    
+    internal interface IWebPDecodeTask
+    {
+        void Excute(int index);
+    }
+
+    internal class WebPDecodeTask : IWebPDecodeTask
+    {
+        // have to use Concurrent structures to prevent concurrent race exceptions
+        private ConcurrentDictionary<int, (byte[], int)> _managedBytes;
+        private IntPtr _decoder;
+
+        public WebPDecodeTask(IntPtr decoder, ConcurrentDictionary<int, (byte[], int)> managedBytes)
+        {
+            _managedBytes = managedBytes;
+            _decoder = decoder;
+        }
+        
+        /// <summary>
+        /// The actual decode process which make the magic happen
+        /// </summary>
+        public void Excute(int index)
+        {
+            // get the demuxer (which contains almost all the information about the WebP file)
+            var demuxer = WebP.Extern.libwebpdemux.WebPAnimDecoderGetDemuxer(_decoder);
+            var iter = new WebPIterator();
+            // use the demuxer and WebPIterator to extract one frame from the WebP data
+            var success = WebP.Extern.libwebpdemux.WebPDemuxGetFrame(demuxer, index + 1, ref iter);
+            if (success == 0)
+            {
+                Debug.LogError($"[WebPDecodeTask] Decode frame data {index} failed");
+                return;
+            }
+            
+            // use native memory marshal to minimize the momory consumption from native memory to csharp managed memory
+            var size = (int) iter.fragment.size;
+            var bytes = new byte[size];
+            Marshal.Copy(iter.fragment.bytes, bytes, 0, size);
+            
+            // parse the memory data (structured bitmap data) to texture bytes, which can be used to parse into Texture
+            var loadedBytes = Texture2DExt.LoadRGBAFromWebP(bytes, ref iter.width, ref iter.height, false, out var error, null);
+            if (error != Error.Success)
+            {
+                Debug.LogError($"[WebPDecodeTask] Decode texture bytes {index} failed");
+                return;
+            }
+            // sequential added to the concurrent dictionary
+            // if not using concurrent dict, exceptions may happen 
+            _managedBytes?.TryAdd(index, (loadedBytes, iter.duration));
+            // release the iterator pointer
+            WebP.Extern.libwebpdemux.WebPDemuxReleaseIterator(ref iter);
+        }
+    }
+
+    /// <summary>
+    /// Convinience for task usage
+    /// </summary>
+    internal struct WebPDecoderJob : IJobParallelFor
+    {
+
+        public GCHandle Task;
+        
+        public void Execute(int index)
+        {
+            var task = (IWebPDecodeTask) Task.Target;
+            task?.Excute(index);
+        }
+    }
+}
---
+++ unity_project/Assets/unity.webp/WebPDecoderWrapper.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using WebP.Extern;
+
+namespace Game
+{
+
+    /// <summary>
+    /// The decod logic before and after decoding.
+    /// </summary>
+    public static class WebPDecoderWrapper
+    {
+
+        public static async Task<List<(Texture2D, int)>> Decode(byte[] bytes)
+        {
+            // create the decoder
+            if (!CreateDecoder(bytes, out var decoder, out var info)) return null;
+            
+            Debug.Log($"[WebPDecoderWrapper] Loaded animation: {info.frame_count}, {info.canvas_width}/{info.canvas_height}");
+            // decode every frame of the WebP file with threads
+            var decodedBytes = await WebPDecodeJob.StartJob(decoder, info.frame_count);
+            
+            Debug.Log($"[WebPDecoderWrapper] Raw bytes decode complete");
+
+            var textures = CreateTexturesFromBytes(decodedBytes, info.canvas_width, info.canvas_height);
+            
+            // release the decoder
+            WebPDemuxBindings.WebPAnimDecoderReset(decoder);
+            WebPDemuxBindings.WebPAnimDecoderDelete(decoder);
+
+            return textures;
+        }
+
+        private static List<(Texture2D, int)> CreateTexturesFromBytes(List<(byte[], int)> decodedBytes, int width, int height)
+        {
+            
+            var textures = new List<(Texture2D, int)>();
+            // turn bytes into texture
+            for (var i = 0; i < decodedBytes.Count; i++)
+            {
+                var (rawBytes, timestamp) = decodedBytes[i];
+
+                if (ConvertBytesToTexture(rawBytes, width, height, out var texture))
+                {
+                    textures.Add((texture, timestamp));
+                }
+                else
+                {
+                    textures.Add(i == 0
+                        ? (new Texture2D(width, height), timestamp)
+                        : (textures[i - 1].Item1, timestamp));
+                }
+            }
+
+            return textures;
+        }
+
+        private static bool ConvertBytesToTexture(byte[] bytes, int width, int height, out Texture2D texture)
+        {
+            if (bytes == null || bytes.Length <= 0)
+            {
+                texture = null;
+                return false;
+            }
+
+            texture = new Texture2D(width, height, TextureFormat.RGBA32, false, false);
+            try
+            {
+                texture.LoadRawTextureData(bytes);
+                texture.Apply(false, true);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"[WebPDecoderWrapper] Bytes to texture error: {e.Message}");
+                texture = null;
+            }
+            
+            return texture != null;
+        }
+        
+        public static unsafe bool CreateDecoder(byte[] bytes, out IntPtr decoder, out WebPAnimInfo info)
+        {
+            decoder = IntPtr.Zero;
+            info = new WebPAnimInfo();
+            if (bytes == null || bytes.Length <= 0) return false;
+
+            fixed (byte* p = bytes)
+            {
+                var webpData = new WebPData
+                {
+                    bytes = (IntPtr) p,
+                    size = (UIntPtr) bytes.Length
+                };
+                var options = new WebPAnimDecoderOptions
+                {
+                    flip = 1, 
+                    use_threads = 1,
+                    color_mode = WEBP_CSP_MODE.MODE_RGBA
+                };
+
+                decoder = WebPDemuxBindings.WebPAnimDecoderNew(ref webpData, ref options);
+                
+                info = new WebPAnimInfo();
+                var success = WebPDemuxBindings.WebPAnimDecoderGetInfo(decoder, ref info);
+
+                if (success == 0)
+                {
+                    Debug.LogError($"[WebPDecoderWrapper] Get file info failed");
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+    }
+}
