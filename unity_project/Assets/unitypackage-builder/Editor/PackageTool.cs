7b9b3a628e78597ac4d4728011b08397bef70de1
netpyoung <netpyoung@gmail.com>
2021-11-20 19:45:47 +0900

    unity.webp 0.3.1 - (libwebp version v1.2.1)

--- unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
+++ unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
@@ -5,7 +5,7 @@ public class PackageTool
     [MenuItem("Package/Update Package")]
     private static void UpdatePackage()
     {
-        const string VERSION = "0.3.0";
+        const string VERSION = "0.3.1";
         AssetDatabase.ExportPackage(
             new[] {"Assets/unity.webp"},
             $"../webp-{VERSION}.unitypackage",
