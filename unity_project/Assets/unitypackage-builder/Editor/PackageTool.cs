2d10bab1e0cc6dc95d7ba6073999c56c1211800f
netpyoung <netpyoung@gmail.com>
2022-04-07 08:14:12 +0900

    unity.webp 0.3.4 - (libwebp version v1.2.2)
    * change: plugin platform settings on MacOSX x86_64 => Any CPU

--- unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
+++ unity_project/Assets/unitypackage-builder/Editor/PackageTool.cs
@@ -5,7 +5,7 @@ public class PackageTool
     [MenuItem("Package/Update Package")]
     private static void UpdatePackage()
     {
-        const string VERSION = "0.3.3";
+        const string VERSION = "0.3.4";
         AssetDatabase.ExportPackage(
             new[] {"Assets/unity.webp"},
             $"../webp-{VERSION}.unitypackage",
